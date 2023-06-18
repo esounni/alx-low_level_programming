80c161ef01d98a18c456ff32e4aa17517a34b1cf
esounni <esounni0@gmail.com>
2023-06-18 08:53:16 -0700

    print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * prints the lowercase alphabet in reverse
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 122; n >= 97; n--)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
